ca3f9e279ad68d046395670a5e7fd0dbc46715a3
menahem_erlich <erlichmenahem@gmail.com>
2026-01-08 12:42:31 +0200

    bonus

--- classes/Ticket.js
+++ classes/Ticket.js
@@ -2,7 +2,15 @@
 export class Ticket{
     constructor(price, ownerName){
         this.ticketNumber = Math.floor(Math.random() * 100)
-        this.price = price
-        this.ownerName = ownerName
+        if (typeof price === "number") {
+            this.price = price
+        } else {
+            throw new Error("invalid price");
+        }
+        if (typeof ownerName === "string") {
+            this.ownerName = ownerName
+        } else {
+            throw new Error("invalid ownerName");
+        }
     }
 }
